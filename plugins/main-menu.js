bb3473e5af81527d8fe4c45c8f4b25bfeb7574b2
ABDULRAHIM-NYONI <nyoniraheem@gmail.com>
2026-01-10 15:03:23 +0300

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -1,51 +1,64 @@
+const config = require('../config');
+const { cmd } = require('../command');
+
 cmd({
-    pattern: "menu",
+    pattern: "menu2",
     desc: "Show bottom menu",
-    category: "menu",
+    category: "menu2",
     react: "🔥",
     filename: __filename
-}, async (conn, mek, m, { from }) => {
+}, async (conn, mek, m, { from, pushName }) => {
     try {
+        const bodyText = `👋 *Welcome ${pushName || "User"}*\n\n🤖 *${config.BOT_NAME || "RAHEEM-XMD"}*\n⚡ Fast • Smart • Powerful\n\nSelect a category below to explore my features:`;
 
-        const caption = `
-👋 *Welcome ${m.pushName || "User"}*
-
-🤖 *${config.BOT_NAME}*
-⚡ Fast • Smart • Powerful
-
-Chagua menu hapa chini 👇
-`;
-
-        const buttons = [
-            {
-                buttonId: `${config.PREFIX}allmenu`,
-                buttonText: { displayText: "📜 ALL MENU" },
-                type: 1
-            },
-            {
-                buttonId: `${config.PREFIX}download`,
-                buttonText: { displayText: "📥 DOWNLOAD" },
-                type: 1
-            },
-            {
-                buttonId: `${config.PREFIX}groupmenu`,
-                buttonText: { displayText: "👥 GROUP" },
-                type: 1
+        const message = {
+            interactiveMessage: {
+                header: {
+                    hasVideoMessage: false,
+                    hasImageMessage: true,
+                    imageMessage: (await conn.prepareMessageMedia({ image: { url: "https://files.catbox.moe/8s7lxh.jpg" } }, { upload: conn.waUploadToServer })).imageMessage,
+                    title: "",
+                    subtitle: "RAHEEM-XMD",
+                },
+                body: { text: bodyText },
+                footer: { text: "© RAHEEM-XMD-3 • 2026" },
+                nativeFlowMessage: {
+                    buttons: [
+                        {
+                            name: "quick_reply",
+                            buttonParamsJson: JSON.stringify({
+                                display_text: "📜 ALL MENU",
+                                id: ".allmenu"
+                            })
+                        },
+                        {
+                            name: "quick_reply",
+                            buttonParamsJson: JSON.stringify({
+                                display_text: "📥 DOWNLOAD",
+                                id: ".download"
+                            })
+                        },
+                        {
+                            name: "quick_reply",
+                            buttonParamsJson: JSON.stringify({
+                                display_text: "👥 GROUP",
+                                id: ".groupmenu"
+                            })
+                        }
+                    ],
+                }
             }
-        ];
-
-        const buttonMessage = {
-            image: { url: "https://files.catbox.moe/8s7lxh.jpg" },
-            caption: caption,
-            footer: "© RAHEEM-XMD-3 • 2026",
-            buttons: buttons,
-            headerType: 4
         };
 
-        await conn.sendMessage(from, buttonMessage, { quoted: mek });
+        // Tunatuma kama viewOnce ili ionekane vizuri kwenye matoleo yote
+        await conn.relayMessage(from, {
+            viewOnceMessage: {
+                message: message
+            }
+        }, {});
 
     } catch (e) {
         console.log(e);
-        await conn.sendMessage(from, { text: "❌ Menu error!" }, { quoted: mek });
+        await conn.sendMessage(from, { text: "❌ System Error: Buttons are not supported on this version of WhatsApp." }, { quoted: mek });
     }
 });
