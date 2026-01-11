24f0d8b213f9fdf6db1f04a8e9f54d8c3488f38d
ABDULRAHIM-NYONI <nyoniraheem@gmail.com>
2026-01-11 22:48:07 +0300

    Create test.js

---
+++ plugins/test.js
@@ -0,0 +1,23 @@
+*✅ COMMAND GENERATED!* 
+
+*Jina:* channel
+
+```javascript
+const { cmd } = require('../command');
+
+cmd({
+    pattern: "channel",
+    desc: "nitumie hiii channel " https",
+    category: "generated",
+    react: "🔥",
+    filename: __filename
+}, async (conn, mek, m, { from, reply, q }) => {
+    try {
+        // Hii command imetengenezwa na MK-XR Maker
+        return reply("Hii ni command ya channel imefanikiwa!");
+    } catch (e) {
+        console.log(e);
+        reply("❌ Error!");
+    }
+});
+```
