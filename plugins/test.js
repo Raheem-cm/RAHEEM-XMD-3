b5d97472e906abb119c34617adab4d737e17365c
ABDULRAHIM-NYONI <nyoniraheem@gmail.com>
2026-01-12 08:51:04 +0300

    Update test.js

--- plugins/test.js
+++ plugins/test.js
@@ -1,16 +1,1 @@
- const { cmd } = require('../command');
-
-cmd({
-    pattern: "channel",
-    desc: "nitumie hiii channel " https://whatsapp.com/channel/0029VbAffhD2ZjChG9DX922r" idadi ya follower wake na jina la channel",
-    category: "manual",
-    react: "✅",
-    filename: __filename
-}, async (conn, mek, m, { from, reply }) => {
-    try {
-        await reply("Hii command ya channel inafanya kazi!");
-    } catch (e) {
-        console.error(e);
-    }
-});
 
