a91282e258a3cfc06133c5559bb7a316c713bf07
ABDULRAHIM-NYONI <nyoniraheem@gmail.com>
2026-01-12 09:02:27 +0300

    Update test.js

--- plugins/test.js
+++ plugins/test.js
@@ -1,1 +1,18 @@
 
+const { cmd } = require('../command');
+
+cmd({
+    pattern: "news",
+    desc: 'Send the latest channel updates',
+    category: "generated",
+    react: "✅",
+    filename: __filename
+}, async (conn, mek, m, { from, reply }) => {
+    try {
+        const responseText = "*MORTAL-KOMBAT-XR SYSTEM*\n\n*Command:* news\n*Task:* Send the latest channel updates";
+        await conn.sendMessage(from, { text: responseText }, { quoted: mek });
+    } catch (e) {
+        console.error(e);
+    }
+});
+
