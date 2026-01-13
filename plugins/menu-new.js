7a98e598e426ea880194ca4b52bdb68adb5fc39d
ABDULRAHIM-NYONI <nyoniraheem@gmail.com>
2026-01-13 20:02:05 +0300

    Update menu-new.js

--- plugins/menu-new.js
+++ plugins/menu-new.js
@@ -283,7 +283,7 @@ cmd({
 `;
 
         await conn.sendMessage(from, {
-            Gif: { url: "https://files.catbox.moe/c08e2d.mp4" },
+            gift: { url: "https://files.catbox.moe/c08e2d.mp4" },
             caption: menuText.trim(),
             contextInfo: {
                 mentionedJid: [m.sender],
